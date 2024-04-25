4325ee2ffe5ede9a435c60b5864f7b029f4e67a8
Elisson Neves <elisson.gnl@gmail.com>
2024-04-25 17:36:34 -0300

    criando o projeto tipos de variaveis

--- tipos-variaveis/src/tiposdevariaveis.java
+++ tipos-variaveis/src/tiposdevariaveis.java
@@ -1,5 +1,28 @@
 public class tiposdevariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+
+        //String é o nome da variavel para representar texto
+        String meuNome = "Elisson";
+        System.out.println(meuNome);
+
+        double salarioMinimo = 1500;
+        System.out.println(salarioMinimo);
+
+        //Mesmo sabendo que a variavel Short tem um valor curto, é necessário que haja a conversão
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroNorma2 = (short) numeroNormal;
+        System.out.println(numeroNorma2);
+
+        //Desta maneira o valor da variavel pode ser alterado
+        int numero = 5;
+        
+        numero = 10;
+        System.out.println(numero);
+
+        //Colocando o "final" o valor não pode ser alterado
+        final double valordePi = 3.14;
+        System.out.println(valordePi);
     }
 }
